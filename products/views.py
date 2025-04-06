c60f81903fa9c67273261c4dc936326fd01041b5
GalkaKG <galina_georgieva_net@abv.bg>
2025-04-06 21:56:45 +0300

    Optimize product listing performance and UI improvements

--- products/views.py
+++ products/views.py
@@ -1,13 +1,34 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.cache import cache_page
 
 from .models import Product, ProductVariant, Cart, CartItem
 
 
+@cache_page(60 * 15)  # Кеширане за 15 минути
 def product_list(request):
-    products = Product.objects.all()
-    return render(request, 'products/product_list.html', {'products': products})
+    # Взимаме параметъра за сортиране от URL
+    sort_by = request.GET.get('sort', '')  # Празно по подразбиране
+
+    # Взимаме всички продукти
+    products = Product.objects.prefetch_related('variants__color').all()
+
+    # Прилагаме сортиране според избраната опция
+    if sort_by == 'price_asc':
+        products = products.order_by('base_price')
+    elif sort_by == 'price_desc':
+        products = products.order_by('-base_price')
+    elif sort_by == 'updated':
+        products = products.order_by('-id')  # Или '-updated_at' ако имате такова поле
+    else:
+        # По подразбиране - първи създадени (най-стари)
+        products = products.order_by('id')
+
+    return render(request, 'products/product_list.html', {
+        'products': products,
+        'current_sort': sort_by
+    })
 
 
 def product_detail(request, product_id):
